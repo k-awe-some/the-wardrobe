2722399f6f14ba6d789d742b0ea8ecfb254e02a1
kay <kaynguyen.dev@gmail.com>
2019-08-20 16:57:43 -0400

    created & implemented Home page into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 // import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+
+import Home from "./pages/home/home.component";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/home/home.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Categories from "../../components/categories/categories.component";
+
+const Home = () => <Categories />;
+
+export default Home;
