9e0c63c5960cadb7a4ddb31f6ac91cfd88a9326e
kay <kaynguyen.dev@gmail.com>
2019-08-21 17:48:02 -0400

    implemented Google sign in authentication into App

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.scss";
 
-import Home from "./pages/home/home.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+// import Home from "./pages/home/home.component";
+// import Shop from "./pages/shop/shop.component";
+import SignInSignOut from "./pages/signin-signout/signin-signout.component";
+
+class App extends React.Component {
+  state = {
+    currentUser: null
+  };
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user =>
+      this.setState({
+        currentUser: user
+      })
+    );
+  }
+
+  render() {
+    const { currentUser } = this.state;
+    console.log(currentUser);
+    return (
+      <div className="App">
+        <SignInSignOut />
+      </div>
+    );
+  }
 }
 
 export default App;
