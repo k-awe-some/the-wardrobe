e04917da3d6ff786c0761658a2ede33a8ab7c3ee
kay <kaynguyen.dev@gmail.com>
2019-08-21 17:44:06 -0400

    implemented FormInput into SignIn

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 
 import { signInWithGoogleMethod } from "../../firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
 
 class SignIn extends React.Component {
   state = {
-    username: "",
+    email: "",
     password: ""
   };
 
   handleSubmit = event => event.preventDefault();
 
   render() {
+    const { email, password } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        Username: <input type="text" />
-        Password: <input type="password" />
-        <button type="submit">Submit</button>
+        <FormInput type="text" name="email" label="Your email" value={email} />
+        <FormInput
+          type="password"
+          name="password"
+          label="Your password"
+          value={password}
+        />
+        <button type="submit">Sign in</button>
         <button onClick={signInWithGoogleMethod}>Sign in with Google</button>
       </form>
     );
