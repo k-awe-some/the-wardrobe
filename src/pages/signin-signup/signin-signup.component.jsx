0af0515465060e8d20ba0f1e4d3d4caf92ecfb34
kay <kaynguyen.dev@gmail.com>
2019-08-22 10:55:30 -0400

    styled SignInSignUp component; retouched index.css

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "./signin-signup.styles.scss";
 
 import SignIn from "../../components/signin/signin.component";
 import SignUp from "../../components/signup/signup.component";
 
 const SignInSignUp = () => (
-  <React.Fragment>
+  <div className="signin-signup">
     <SignIn />
     <SignUp />
-  </React.Fragment>
+  </div>
 );
 
 export default SignInSignUp;
