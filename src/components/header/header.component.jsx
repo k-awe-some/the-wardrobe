e9c01f19410801e19b44b6d53aa540e07fc91e60
kay <kaynguyen.dev@gmail.com>
2019-08-22 10:35:51 -0400

    styled Header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./header.styles.scss";
 
 import { auth } from "../../firebase/firebase.utils";
 
 const Header = ({ currentUser }) => (
-  <React.Fragment>
-    <ul>
-      <Link to="/">HOME</Link>
-      <li>SHOP</li>
-      <li>CONTACT</li>
-      <Link to="/signin">
-        {currentUser ? (
-          <div onClick={() => auth.signOut()}>SIGN OUT</div>
-        ) : (
-          <div>SIGN IN</div>
-        )}
-      </Link>
-    </ul>
-  </React.Fragment>
+  <div className="header">
+    <Link to="/" className="header__item">
+      HOME
+    </Link>
+    <Link to="/shop" className="header__item">
+      SHOP
+    </Link>
+    <Link to="/contact" className="header__item">
+      CONTACT
+    </Link>
+    <div className="header__item">
+      {currentUser ? (
+        <div onClick={() => auth.signOut()}>SIGN OUT</div>
+      ) : (
+        <Link to="/signin">SIGN IN</Link>
+      )}
+    </div>
+  </div>
 );
 
 export default Header;
