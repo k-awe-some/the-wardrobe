80f7107aa34e8b218867c11d30f43c2e86194591
kay <kaynguyen.dev@gmail.com>
2019-08-24 12:51:34 -0400

    implemented CustomButton into CollectionItem; retouched styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
 const CollectionItem = ({ item }) => (
   <div className="preview-item" key={item.id}>
     <div
       className="preview-item--background"
       style={{ backgroundImage: `url('${item.imageUrl}')` }}
-    ></div>
+    >
+      <CustomButton text="add to cart" />
+    </div>
+
     <div className="preview-item--info">
       <span>{item.name}</span>
       <span>${item.price}.00</span>
